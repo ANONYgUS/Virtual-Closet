c8073011460f6b0beb416b5c670f9f68600b4abd
Niklas <nkniesche1@gmail.com>
2017-11-10 22:44:14 -0500

    fixed merge issue

--- Fashion/FrontConnectionMode.java
+++ Fashion/FrontConnectionMode.java
@@ -2,12 +2,9 @@
 
 public enum FrontConnectionMode{
 
-<<<<<<< HEAD
+
   UNZIPPED(5, new Formality(0,7,0.3)),
-  ZIPPED()
-=======
-  UNZIPPED(5, new Formality(0,7,0.3));
->>>>>>> 1ac9b9be8206465f19c43c3c001653dab5bbd061
+  ZIPPED(5, new Formality(0,7,0.3)); //temp values
 
   private int temperature;
   private Formality formality;
